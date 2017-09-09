3fb79c0d3bf955fbd37f83e7698c6b5b70e8a00a
Audrey Lim <audim98@icloud.com>
2017-09-09 16:56:29 +0800

    Initial Commit

---
+++ NextChat/Contact.swift
@@ -0,0 +1,9 @@
+//
+//  Contact.swift
+//  NextChat
+//
+//  Created by Audrey Lim on 09/09/2017.
+//  Copyright © 2017 Audrey Lim. All rights reserved.
+//
+
+import Foundation
---
+++ NextChat/ContactTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ContactTableViewCell.swift
+//  NextChat
+//
+//  Created by Audrey Lim on 09/09/2017.
+//  Copyright © 2017 Audrey Lim. All rights reserved.
+//
+
+import UIKit
+
+class ContactTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
