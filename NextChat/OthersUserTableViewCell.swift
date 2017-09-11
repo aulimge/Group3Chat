9785d9893364afcc4ef03a054dc2c9aa199cdfce
Hoang Thu Ha <hoangthuha880@gmail.com>
2017-09-11 12:04:58 +0800

    edit User VC

---
+++ NextChat/OthersUserTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  OthersUserTableViewCell.swift
+//  NextChat
+//
+//  Created by Hoang Thu Ha on 11/9/17.
+//  Copyright © 2017 Audrey Lim. All rights reserved.
+//
+
+import UIKit
+
+class OthersUserTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
