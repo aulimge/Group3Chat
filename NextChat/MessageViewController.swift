733337933130e75254674c9b57db83749d2eeb69
Hoang Thu Ha <hoangthuha880@gmail.com>
2017-09-11 11:35:39 +0800

    outlet Message VC

--- NextChat/MessageViewController.swift
+++ NextChat/MessageViewController.swift
@@ -3,6 +3,17 @@ import UIKit
 class MessageViewController: UIViewController {
     
     @IBOutlet weak var messageName: UILabel!
+    
+    @IBOutlet weak var userName: UILabel!
+    
+    @IBOutlet weak var msgUserName: UITextView!
+    
+    @IBOutlet weak var othersUserName: UILabel!
+    
+    @IBOutlet weak var msgOthersUserName: UITextView!
+    
+    @IBOutlet weak var timeStamp: UITextField!
+    
     var selectedProfileContact : Contact?
     
     override func viewDidLoad() {
