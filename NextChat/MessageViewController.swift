284a62956c745c00c0cf4173f7c4c46e27d62490
Hoang Thu Ha <hoangthuha880@gmail.com>
2017-09-11 10:55:32 +0800

    edit Message VC

--- NextChat/MessageViewController.swift
+++ NextChat/MessageViewController.swift
@@ -1,35 +1,19 @@
-//
-//  MessageViewController.swift
-//  NextChat
-//
-//  Created by Audrey Lim on 09/09/2017.
-//  Copyright © 2017 Audrey Lim. All rights reserved.
-//
-
 import UIKit
 
 class MessageViewController: UIViewController {
-
+    
+    @IBOutlet weak var messageName: UILabel!
+    var selectedProfileContact : Contact?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        guard let nameMessage = selectedProfileContact?.name
+            else {return}
+        
+        messageName.text = nameMessage
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
